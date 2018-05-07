ed92fb0dba90b6de52b6cdf0accb0c87b976e0bb
Uri Shaked <uri@urishaked.com>
2018-05-07 23:01:42 +0300

    Initial commit

---
+++ src/extension.ts
@@ -0,0 +1,113 @@
+'use strict';
+
+require('util.promisify/shim')();
+
+import * as vscode from 'vscode';
+import * as path from 'path';
+import { tsquery } from '@phenomnomnominal/tsquery';
+import { getProgram } from 'typewiz-core';
+import { createCompilerHost, Node } from 'typescript';
+
+interface IResult extends vscode.QuickPickItem {
+  nodes: Node[];
+}
+
+function highlightNode(editor: vscode.TextEditor, node: Node) {
+  const startPos = editor.document.positionAt(node.getStart());
+  const endPos = editor.document.positionAt(node.getEnd());
+  editor.selection = new vscode.Selection(startPos, endPos);
+  editor.revealRange(new vscode.Range(startPos, endPos));
+}
+
+async function showResults(matches: Node[][]) {
+  const filenames = matches.map(
+    item =>
+      ({
+        label: vscode.workspace.asRelativePath(item[0].getSourceFile().fileName),
+        description: `${item.length} matches`,
+        nodes: item,
+      } as IResult),
+  );
+
+  const result = await vscode.window.showQuickPick(filenames);
+  if (result) {
+    const firstMatch = result.nodes[0];
+    const editor = await vscode.window.showTextDocument(vscode.Uri.file(firstMatch.getSourceFile().fileName));
+    highlightNode(editor, firstMatch);
+  }
+}
+
+async function astQueryFile() {
+  const editor = vscode.window.activeTextEditor;
+  if (!editor || ['javascript', 'typescript'].indexOf(editor.document.languageId) < 0) {
+    vscode.window.showErrorMessage('AST Queries only supported for TypeScript and JavaScript files');
+    return;
+  }
+
+  const astQuery = await vscode.window.showInputBox({
+    prompt: 'AST Query to search for',
+    placeHolder: 'e.g. Constructor',
+  });
+  if (astQuery) {
+    const ast = tsquery.ast(editor.document.getText(), editor.document.fileName);
+    const nodes = tsquery(ast, astQuery);
+    if (nodes.length) {
+      highlightNode(editor, nodes[0]);
+    } else {
+      vscode.window.showInformationMessage('No results found :-(');
+    }
+  }
+}
+
+async function astQueryWorkspace() {
+  const tsconfigFiles = await vscode.workspace.findFiles('tsconfig.json');
+  if (!tsconfigFiles.length) {
+    vscode.window.showErrorMessage('Could not find any tsconfig.json file in your project');
+  }
+  if (tsconfigFiles.length > 1) {
+    console.error('TODO: Implement quick pick here');
+  }
+
+  const astQuery = await vscode.window.showInputBox({
+    prompt: 'AST Query to search for',
+    placeHolder: 'e.g. Constructor',
+  });
+  if (astQuery) {
+    const tsconfigPath = tsconfigFiles[0].fsPath;
+    const program = getProgram({
+      rootDir: path.dirname(tsconfigPath),
+      tsConfig: tsconfigPath,
+      tsCompilerHost: createCompilerHost({}, true),
+    });
+
+    if (!program) {
+      vscode.window.showErrorMessage('Could not process typescript project');
+      return;
+    }
+
+    const matches: Node[][] = [];
+    for (let source of program.getSourceFiles()) {
+      const nodes = tsquery(source, astQuery);
+      if (nodes.length) {
+        matches.push(nodes);
+      }
+    }
+
+    if (matches.length) {
+      showResults(matches);
+    } else {
+      vscode.window.showInformationMessage('No results found :-(');
+    }
+  }
+}
+
+export function activate(context: vscode.ExtensionContext) {
+  context.subscriptions.push(
+    ...[
+      vscode.commands.registerCommand('extension.astQueryFile', astQueryFile),
+      vscode.commands.registerCommand('extension.astQueryWorkspace', astQueryWorkspace),
+    ],
+  );
+}
+
+export function deactivate() {}
